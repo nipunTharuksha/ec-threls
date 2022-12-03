a90fa2f98c660d2f464fd23be96c1e25a4096bfc
nipuntharuksha <nipunth7@gmail.com>
2022-12-03 13:08:13 +0530

    changes

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,15 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Models\Brand
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Brand newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Brand newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Brand query()
- * @mixin \Eloquent
- */
 class Brand extends Model
 {
-    use HasFactory;
+   protected $guarded = [];
 }
